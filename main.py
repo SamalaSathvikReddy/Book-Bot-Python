fa21a73ef75a16f645aa3ec3b3943d9c3b47131d
SamalaSathvikReddy <samalasathvikreddy7002@gmail.com>
2025-09-23 21:11:58 +0530

    The final Version of this Project.
    :)

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from stats import get_num_words
+import sys
+import stats
 
 def get_book_text(path):
     with open(path, "r") as f:
         file_contents = f.read()
     return file_contents
 
+
+def get_stats(path):
+    main_text = get_book_text(path)
+    print("============ BOOKBOT ============") 
+    print(f"Analyzing the book found at {path}....")
+    word_count = stats.get_num_words(main_text)
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    freq = stats.char_count(main_text)
+    print("--------- Character Count -------") 
+    
+    for i in freq:
+        if i[0].isalpha() == True: 
+            print(f"{i[0]} : {i[1]}")
+    print("============= END ===============")
+
 def main():
-    main_txt = get_book_text("books/frankenstein.txt")
-    print(main_txt)
-    print(f"{get_num_words(main_txt)} words are there in document")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    get_stats(sys.argv[1])     
+    
 main()
 
 
--- stats.py
+++ stats.py
@@ -3,4 +3,13 @@ def get_num_words(text):
     num = len(text)
     return num
 
-
+def char_count(text):
+    freq = dict()
+    for words in text:
+        for letter in words:
+            if (letter.lower()) in freq:
+                freq[(letter.lower())] += 1
+            else:
+                freq[(letter.lower())] = 1
+    freq = sorted(freq.items(), key=lambda item:item[1], reverse = True) 
+    return freq
